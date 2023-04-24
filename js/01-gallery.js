ecd8e2679efb232c5e42087d3bb97fc8fd2f65f3
tonejman <nej.man@interia.pl>
2023-04-24 14:05:10 +0200

    task completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+/* eslint-disable no-undef */
+/* eslint-disable no-use-before-define */
+/* eslint-disable quotes */
+/* eslint-disable import/extensions */
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const galleryEl = createGalleryEl(galleryItems);
+gallery.innerHTML = galleryEl;
+
+function createGalleryEl() {
+  return galleryItems
+
+    // eslint-disable-next-line arrow-body-style
+    .map(({ preview, original, description }) => {
+      return `<div 
+      class="gallery__item">
+      <a class="gallery__link"
+      href="${original}">
+      <img class="gallery__image"
+      src=" ${preview}" 
+      data-source="${original}" alt=" 
+      ${description}"/>
+      </a>
+      </div>`;
+    })
+
+    .join("");
+}
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+  );
+  instance.show();
+
+  const visible = basicLightbox.visible();
+  if (visible) {
+    gallery.addEventListener("keydown", (evt) => {
+      if (evt.key === "Escape") {
+        instance.close();
+      }
+    });
+  }
+});
